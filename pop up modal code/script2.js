230d43862a15dbf714ed2405334c56066933fd65
markkaytavion <ferguson.devontae@gmail.com>
2022-06-26 18:08:03 -0400

    About me page mvp.. added pop up modal.. no resume yet

---
+++ pop up modal code/script2.js
@@ -0,0 +1,11 @@
+const openModal = document.getElementById("open")
+const modalContainer = document.getElementById("modal_container")
+const closeModal = document.getElementById("close")
+
+openModal.addEventListener('click', () => {
+    modalContainer.classList.add('show')
+});
+
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('show')
+});
--- script.aboutme.js
+++ script.aboutme.js
@@ -30,3 +30,16 @@ function expand() {
     }
 }
 //Navigation^^
+//Modal Button
+const openModal = document.getElementById("open")
+const modalContainer = document.getElementById("modal_container")
+const closeModal = document.getElementById("close")
+
+openModal.addEventListener('click', () => {
+    modalContainer.classList.add('show')
+});
+
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('show')
+});
+//modal button^^
